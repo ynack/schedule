465bd96d4658929ba3ae0752c56370e680268c82
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-20 10:49:59 +0900

    Create year.js

---
+++ js/year.js
@@ -0,0 +1,4 @@
+document.write("<option value=''></option>");
+document.write("<option value='2019'>2019</option>");
+document.write("<option value='2020'>2020</option>");
+document.write("<option value='2021'>2021</option>");

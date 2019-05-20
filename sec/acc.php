f900ce36e290e8f4414fd3373324796d7c1adfa6
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-20 12:12:30 +0900

    Create acc.php

---
+++ sec/acc.php
@@ -0,0 +1,9 @@
+<?php
+
+/*	データベース接続	*/
+	$dsn = "mysql:dbname=department;host=macbookpro.local";
+	//$dsn = "mysql:dbname=department;host=35.247.121.229";
+	$user = "admin";
+	$pass = "admin";
+
+?>

cb455af8a515d5ae65332f82117dd0654e974ef3
Yosuke Nakayama <48463756+ynack@users.noreply.github.com>
2019-05-23 13:26:23 +0900

    Delete acc.php

--- sec/acc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*	データベース接続	*/
-	$dsn = "mysql:dbname=department;host=macbookpro.local";
-	//$dsn = "mysql:dbname=department;host=35.247.121.229";
-	$user = "admin";
-	$pass = "admin";
-
-?>

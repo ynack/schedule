901e3279fa31239911e856a248f13bed5d84f5e6
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-23 18:54:03 +0900

    tiny update

--- login.php
+++ login.php
@@ -5,40 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>勤怠管理　試用版</title>
 		<script src="./js/jquery.js"></script>
-		<?php
-			$ua = $_SERVER["HTTP_USER_AGENT"];
-			if(strpos($ua,"iPhone"))
-			{
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/iphone/header.css\" />\n";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/iphone/main.css\" />\n";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/iphone/ui.css\" />\n";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/iphone/layout.css\" />\n";
-			}
-			else if(strpos($ua,"Android"))
-			{
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/android/header.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/android/main.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/android/ui.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/android/table.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/android/layout.css\" />"; 
-			}
-			else if(strpos($ua,"Windows"))
-			{
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/win/header.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/win/main.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/win/ui.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/win/table.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/win/layout.css\" />"; 
-			}
-			else
-			{
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/header.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/main.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/ui.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/table.css\" />";
-				echo "<link rel=\"stylesheet\" href=\"./css/Clndr/layout.css\" />"; 
-			}
-		?>	
+		<?php	include("../include_php/ua_css.php"); ?>
 		<link rel="stylesheet" href="../css/font/style.css" />
 	</head>
 	<body>
